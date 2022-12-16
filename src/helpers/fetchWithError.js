5456fdac31e92ec667232c396ba645e90a81ccc5
Server Khalilov <red17electro@gmail.com>
2022-12-16 17:08:43 +0100

    upd: error handling;

---
+++ src/helpers/fetchWithError.js
@@ -0,0 +1,14 @@
+export const fetchWithError = async (url, options) => {
+    const response = await fetch(url, options);
+
+    if (response.status === 200) {
+        const result = await response.json();
+
+        if (result.error) {
+            throw new Error(result.error);
+        }
+        return result;
+    }
+
+    throw new Error(`Error ${response.status}: ${response.statusText}`);
+}
