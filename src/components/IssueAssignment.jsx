c8428c95494cf188d843d823c238c5f9c8561065
Server Khalilov <red17electro@gmail.com>
2022-12-20 14:28:22 +0100

    upd: changing the assignment;

---
+++ src/components/IssueAssignment.jsx
@@ -0,0 +1,63 @@
+import {useUserData} from "../helpers/useUserData";
+import {GoGear} from "react-icons/all";
+import {useState} from "react";
+import {useMutation, useQuery, useQueryClient} from "react-query";
+
+export const IssueAssignment = (({assignee, issueNumber}) => {
+  const user = useUserData(assignee);
+  const [menuOpen, setMenuOpen] = useState(false);
+  const usersQuery = useQuery(["users"], ({signal}) => {
+    return fetch(`/api/users`, {signal}).then((res) => res.json());
+  });
+
+    const queryClient = useQueryClient();
+    const setAssignment = useMutation((assignee) => {
+        return fetch(`/api/issues/${issueNumber}`, {
+            method: "PUT",
+            headers: {
+                "content-type": "application/json"
+            },
+            body: JSON.stringify({assignee})
+        }).then(res => res.json());
+    }, {
+        onMutate: (assignee) => {
+            const oldAssignee = queryClient.getQueryData(["issues", issueNumber]).assignee;
+            queryClient.setQueryData(["issues", issueNumber], (data) => {
+                return {...data, assignee: assignee};
+            });
+
+            return function rollback() {
+                queryClient.setQueryData(["issues", issueNumber], (data) => {
+                    return {...data, assignee: oldAssignee};
+                });
+            }
+        },
+        onError: (error, variables, rollback) => {
+            rollback();
+        },
+        onSettled: () => {
+            queryClient.invalidateQueries(["issues", issueNumber], { exact: true });
+        }
+    });
+
+    return <div className="issue-options">
+      <div>
+          <span>Assignment</span>
+          {
+              user.isSuccess && <div>
+                    <img src={user.data.profilePictureUrl} alt="Assignee Avatar" />
+                  {user.data.name}
+              </div>
+          }
+      </div>
+    <GoGear onClick={() => !usersQuery.isLoading && setMenuOpen(true)}/>
+        {menuOpen && <div className="menu">
+            {usersQuery.data?.map((user) => {
+                return <div key={user.id} onClick={()=>setAssignment.mutate(user.id)}>
+                    <img src={user.profilePictureUrl}/>
+                    {user.name}
+                </div>
+            })}
+        </div>}
+  </div>
+});
--- src/components/IssueDetails.jsx
+++ src/components/IssueDetails.jsx
@@ -4,6 +4,7 @@ import { relativeDate } from "../helpers/relativeDate";
 import { useUserData } from "../helpers/useUserData";
 import { IssueHeader } from "./IssueHeader";
 import {IssueStatus} from "./IssueStatus";
+import {IssueAssignment} from "./IssueAssignment";
 
 function useIssueData(issueNumber) {
   return useQuery(["issues", issueNumber], ({ signal }) => {
@@ -72,6 +73,7 @@ export default function IssueDetails() {
             </section>
             <aside>
               <IssueStatus status={issueQuery.data.status} issueNumber={issueQuery.data.number.toString()}/>
+              <IssueAssignment assignee={issueQuery.data.assignee} issueNumber={issueQuery.data.number.toString()}/>
             </aside>
           </main>
         </>
